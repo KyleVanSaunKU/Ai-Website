8ddfe2d3c3c9f7c73b7ddedd1cd689ce8cc6aa87
Kyle VanSaun <kavansaun@ku.edu>
2024-10-07 22:33:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from openai import OpenAI
 import os
 
 key = os.environ["api_key"]
-client = OpenAI(api_key=key)
-
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {
-            "role": "user",
-            "content": "Write a haiku about recursion in programming."
-        }
-    ]
-)
-
-print(completion.choices[0].message)
+print(key)
